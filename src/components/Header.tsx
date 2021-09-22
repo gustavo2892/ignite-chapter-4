19de40d5b307c5eb80713dfa79c8661c20d18a15
gustavo2892 <gustavo2892@gmail.com>
2021-09-22 12:07:15 -0300

    Options User Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Text, Input, Icon } from '@chakra-ui/react';
-import { RiSearchLine } from 'react-icons/ri';
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from '@chakra-ui/react';
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri';
 
 export function Header() {
   return(
@@ -44,7 +44,36 @@ export function Header() {
           placeholder="Buscar na plataforma"
           _placeholder={{ color: 'gray.400' }}
         />
-        <Icon as={RiSearchLine} fontSize={20}/>
+        <Icon as={RiSearchLine} fontSize="20"/>
+      </Flex>
+
+      <Flex
+        align="center"
+        ml="auto"
+      >
+        <HStack
+          spacing="8"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20"/>
+          <Icon as={RiUserAddLine} fontSize="20"/>
+        </HStack>
+
+        <Flex align="center">
+          <Box mr="4" textAlign="right">
+            <Text>Gustavo Freitas</Text>
+            <Text color="gray.300" fontSize="small">
+              gustavo2892@gmail.com
+            </Text>
+          </Box>
+
+          <Avatar size="md" name="Gustavo Freitas" src="https://github.com/gustavo2892.png"/>
+        </Flex>
       </Flex>
     </Flex>
   )
