cbc4ecc0458dce4157acf1afb334853bde8ebe6b
Gustavo Vidal de Freitas <gustavo2892@gmail.com>
2021-11-07 11:41:54 -0300

    Install and use react-query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -13,26 +13,29 @@ import {
   Td,
   Checkbox,
   Text,
-  useBreakpointValue
+  useBreakpointValue,
+  Spinner
 } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 import Link from 'next/link';
+import { useQuery } from 'react-query';
 
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
 import { Pagination } from "../../components/Pagination";
 
 export default function UserList() {
+  const { data, isLoading, error } = useQuery('users', async () => {
+    const response = await fetch('http://localhost:3000/api/users');
+    const data = await response.json();
+    
+    return data;
+  })
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true
   })
-
-  useEffect(() => {
-    fetch('http://localhost:3000/api/users')
-      .then(response => response.json())
-      .then(data => console.log(data))
-  }, [])
 
   return (
     <Box>
@@ -61,58 +64,71 @@ export default function UserList() {
               </Button>
             </Link>
           </Flex>
-        
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={["4", "4", "6"]} color="gray.300" width="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usuário</Th>
-                {
-                  isWideVersion && <Th>Data de cadastro</Th>
-                }
-                <Th width="8"/>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink"/>
-                </Td>
-                <Td>
-                  <Box>
-                    <Text
-                      fontWeight="bold"
-                    >
-                      Gustavo Freitas
-                    </Text>
-                    <Text fontSize="sm" color="gray.300">
-                      gustavo2892@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {
-                  isWideVersion && <Td>04 de Abril, 2021</Td>
-                }
-                {
-                  isWideVersion &&
-                  <Td>
-                    <Button
-                      as="a"
-                      size="sm"
-                      colorScheme="purple"
-                      leftIcon={<Icon as={RiPencilLine}/>}
-                      fontSize="16"
-                    >
-                      Editar
-                    </Button>
-                  </Td>
-                }
-              </Tr>
-            </Tbody>
-          </Table>
-          <Pagination />
+        {
+          isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex>
+              <Text>Falha ao obter dados do usuário</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                      <Checkbox colorScheme="pink" />
+                    </Th>
+                    <Th>Usuário</Th>
+                    {
+                      isWideVersion && <Th>Data de cadastro</Th>
+                    }
+                    <Th width="8"/>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td px={["4", "4", "6"]}>
+                      <Checkbox colorScheme="pink"/>
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text
+                          fontWeight="bold"
+                        >
+                          Gustavo Freitas
+                        </Text>
+                        <Text fontSize="sm" color="gray.300">
+                          gustavo2892@gmail.com
+                        </Text>
+                      </Box>
+                    </Td>
+                    {
+                      isWideVersion && <Td>04 de Abril, 2021</Td>
+                    }
+                    {
+                      isWideVersion &&
+                      <Td>
+                        <Button
+                          as="a"
+                          size="sm"
+                          colorScheme="purple"
+                          leftIcon={<Icon as={RiPencilLine}/>}
+                          fontSize="16"
+                        >
+                          Editar
+                        </Button>
+                      </Td>
+                    }
+                  </Tr>
+                </Tbody>
+              </Table>
+              <Pagination />
+            </>
+          )}
+          
         </Box>
       </Flex>
     </Box>
