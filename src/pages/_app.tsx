021989330f36c84f9cc31b77fc0f35fe262e647d
gustavo2892 <gustavo2892@gmail.com>
2021-11-14 20:28:35 -0300

    Prefetch React Query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,15 @@ import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import { ReactQueryDevtools } from 'react-query/devtools'
 
-import { QueryClientProvider, QueryClient } from 'react-query';
+import { QueryClientProvider } from 'react-query';
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext';
 import { theme } from '../styles/theme';
 import { makeServer } from '../services/mirage';
+import { queryClient } from '../services/queryClient';
 
 if (process.env.NODE_ENV === 'development') {
   makeServer();
 }
-
-const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,13 +1,15 @@
 import {
-  Box, Button, Checkbox, Flex, Heading, Icon, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue
+  Box, Button, Checkbox, Flex, Heading, Icon, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue, Link
 } from "@chakra-ui/react";
-import Link from 'next/link';
+import NextLink from 'next/link';
 import { useState } from "react";
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
+import { api } from "../../services/api";
 import { useUsers } from "../../services/hooks/useUsers";
+import { queryClient } from "../../services/queryClient";
 
 export default function UserList() {
   const [page, setPage] = useState(1);
@@ -17,6 +19,16 @@ export default function UserList() {
     base: false,
     lg: true
   })
+ 
+  async function handlePrefetchUser(userId: number) {
+    await queryClient.prefetchQuery(['user', userId], async () => {
+      const response = await api.get(`users/${userId}`);
+
+      return response.data;
+    }, {
+      staleTime: 1000 * 60 * 10, // 10 minutos
+    });
+  }
 
   return (
     <Box>
@@ -37,7 +49,7 @@ export default function UserList() {
               }
             </Heading>
 
-            <Link href="/users/create" passHref>
+            <NextLink href="/users/create" passHref>
               <Button
                 as="a"
                 size="sm"
@@ -47,7 +59,7 @@ export default function UserList() {
               >
                 Criar novo
               </Button>
-            </Link>
+            </NextLink>
           </Flex>
         {
           isLoading ? (
@@ -86,7 +98,9 @@ export default function UserList() {
                               <Text
                                 fontWeight="bold"
                               >
-                                {user.name}
+                                <Link color="purple.400" onMouseEnter={() => handlePrefetchUser(user.id)}>
+                                  {user.name}
+                                </Link>
                               </Text>
                               <Text fontSize="sm" color="gray.300">
                                 {user.email}
